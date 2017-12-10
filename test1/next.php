6f5215fb2b8d90f51a9bf1582980cae6bcc2d1c5
David Lázňovský <laznovskylaso@gmail.com>
2017-12-11 00:11:06 +0100

    obrazky na radiobuttony

--- test1/next.php
+++ test1/next.php
@@ -1,24 +1,60 @@
 <head>
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <title>Test 2</title>
-<link rel="stylesheet" type="text/css" href="../style.css">   
+<link rel="stylesheet" type="text/css" href="../style.css">
 </head>
 
-    
+<style>
+label > input{ /* HIDE RADIO */
+  visibility: hidden; /* Makes input not-clickable */
+  position: absolute; /* Remove input from document flow */
+}
+label > input + img{ /* IMAGE STYLES */
+  cursor:pointer;
+  border:2px solid transparent;
+}
+label > input:checked + img{ /* (RADIO CHECKED) IMAGE STYLES */
+  border:2px solid #bbb;
+}
+</style>
 <?php
     ini_set("default_charset", "UTF-8");
     if(isset($_POST["next"]))
     {?>
     	<div id=tst align=center>
-    	<br>Která značka nebyla v obrázcích?<br>
+    	<h2><br>Která značka nebyla v obrázcích?<br></h2>
 
         <form action="next1.php" method="post" name="next1">
-    	
-        1.<input type="radio" name="answer" value="0"><img src="../obr/obr11.png" width="100" height="100"><br>
-    	2.<input type="radio" name="answer" value="1"><img src="../obr/obr3.png" width="100" height="100"><br>
-    	3.<input type="radio" name="answer" value="0"><img src="../obr/obr7.png" width="100" height="100"><br>
-    	4.<input type="radio" name="answer" value="0"><img src="../obr/obr9.png" width="100" height="100"><br>
-    	5.<input type="radio" name="answer" value="0"><img src="../obr/obr8.gif" width="100" height="100"><br>
+
+      <label>
+        <input type="radio" name="answer" value="0">
+        <img src="../obr/obr11.png" width="100" height="100">
+      </label>
+      <br>
+      
+    <label>
+    	<input type="radio" name="answer" value="1">
+      <img src="../obr/obr3.png" width="100" height="100">
+    </label>
+    <br>
+
+    <label>
+    	<input type="radio" name="answer" value="0">
+      <img src="../obr/obr7.png" width="100" height="100">
+    </label>
+    <br>
+
+    <label>
+    	<input type="radio" name="answer" value="0">
+      <img src="../obr/obr9.png" width="100" height="100">
+    </label>
+    <br>
+
+    <label>
+    	<input type="radio" name="answer" value="0">
+      <img src="../obr/obr8.gif" width="100" height="100">
+    </label>
+    <br>
 
         <input type="submit" name="next1" value="Odpovědět" />
         </form>
--- test1/next8.php
+++ test1/next8.php
@@ -1,28 +1,62 @@
 <head>
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <title>Test 2</title>
-<link rel="stylesheet" type="text/css" href="../style.css">   
+<link rel="stylesheet" type="text/css" href="../style.css">
 </head>
+<style>
+label > input{ /* HIDE RADIO */
+  visibility: hidden; /* Makes input not-clickable */
+  position: absolute; /* Remove input from document flow */
+}
+label > input + img{ /* IMAGE STYLES */
+  cursor:pointer;
+  border:2px solid transparent;
+}
+label > input:checked + img{ /* (RADIO CHECKED) IMAGE STYLES */
+  border:2px solid #bbb;
+}
+</style>
 
-   
+
 <?php
     ini_set("default_charset", "UTF-8");
     if(isset($_POST["next8"]))
     {?>
-     
+
 <div id=tst align=center>
-        <br>Jaká SPZ byla na obrázku?<br>
+      <h2>  <br>Jaká SPZ byla na obrázku?<br></h2>
 
         <form action="next9.php" method="post" name="next9">
-        
-       
-        <input type="radio" name="answer" value="0"><img src="../obr/cz3.png" width="192" height="50"> <br>
-        <input type="radio" name="answer" value="0"> <img src="../obr/spz3.jpg" width="192" height="50"><br>
-       <input type="radio" name="answer" value="0"> <img src="../obr/spz4.jpg" width="192" height="50"><br>
-       <input type="radio" name="answer" value="0"><img src="../obr/spz2.jpg" width="192" height="50"><br>
-        <input type="radio" name="answer" value="0"> <img src="../obr/cz2.png" width="192" height="50"><br>
+
+      <label>
+        <input type="radio" name="answer" value="0">
+        <img src="../obr/cz3.png" width="192" height="50">
+      </label>
+      <br>
+      <label>
+        <input type="radio" name="answer" value="0">
+        <img src="../obr/spz3.jpg" width="192" height="50">
+      </label>
+      <br>
+      <label>
+       <input type="radio" name="answer" value="0">
+        <img src="../obr/spz4.jpg" width="192" height="50">
+     </label>
+     <br>
+      <label>
+       <input type="radio" name="answer" value="0">
+       <img src="../obr/spz2.jpg" width="192" height="50">
+     </label>
+     <br>
+
+     <label>
+        <input type="radio" name="answer" value="0">
+        <img src="../obr/cz2.png" width="192" height="50">
+      </label>
+      <br>
+
         <input type="radio" name="answer" value="1">Jiná.<br>
-        
+
         <input type="submit" name="next9" value="Odpovědět" />
         </form>
 </div>
